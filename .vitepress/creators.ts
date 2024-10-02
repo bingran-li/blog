b2d423ccd5149d5d49f97ac439a41751810ac520
Bingran LI <noch_zarathustra@outlook.com>
2024-10-02 21:44:57 +0800

    Update creators.ts

--- .vitepress/creators.ts
+++ .vitepress/creators.ts
@@ -20,14 +20,13 @@ const getAvatarUrl = (name: string) => `https://github.com/${name}.png`
 
 export const creators: Creator[] = [
   {
-    name: '絢香猫',
+    name: 'Bingran Li',
     avatar: '',
     username: 'nekomeowww',
     title: 'Nólëbase 原始创作者',
     desc: '开发者，专注于基础设施维护，数据分析，后端、DevOps 开发',
     links: [
-      { type: 'github', icon: 'github', link: 'https://github.com/nekomeowww' },
-      { type: 'twitter', icon: 'twitter', link: 'https://twitter.com/ayakaneko' },
+      { type: 'github', icon: 'github', link: 'https://github.com/bingran-li' },
     ],
     nameAliases: ['nekomeowww', '绚香猫', '絢香猫', 'Neko Ayaka', 'Ayaka Neko'],
     emailAliases: ['neko@ayaka.moe'],
