5874b3bbe577bb1af5f89965d22eb8657da4fd0a
Bingran LI <noch_zarathustra@outlook.com>
2024-10-02 23:01:58 +0800

    Update index.ts

--- metadata/index.ts
+++ metadata/index.ts
@@ -9,7 +9,7 @@ export const include = ['笔记', '生活']
 /** Repo */
 export const githubRepoLink = 'https://github.com/bingran-li'
 /** Discord */
-export const discordLink = 'https://discord.gg/qVT8zrUQ'
+export const discordLink = 'https://discord.gg/rnXfukyyZ9'
 
 /** 无协议前缀域名 */
 export const plainTargetDomain = 'nolebase.ayaka.io'
