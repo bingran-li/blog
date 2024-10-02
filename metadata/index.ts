5b8f708b4807df57b33d00731bd262bd45fa58ae
Bingran LI <noch_zarathustra@outlook.com>
2024-10-02 23:01:11 +0800

    Update index.ts

--- metadata/index.ts
+++ metadata/index.ts
@@ -1,15 +1,15 @@
 /** 文本 */
-export const siteName = 'Nólëbase'
-export const siteShortName = 'Nólëbase'
-export const siteDescription = '记录回忆，知识和畅想的地方'
+export const siteName = '李炳然的博客'
+export const siteShortName = 'bingran\'s blog'
+export const siteDescription = '数学&哲学与人生'
 
 /** 文档所在目录 */
 export const include = ['笔记', '生活']
 
 /** Repo */
-export const githubRepoLink = 'https://github.com/nolebase/nolebase'
+export const githubRepoLink = 'https://github.com/bingran-li'
 /** Discord */
-export const discordLink = 'https://discord.gg/XuNFDcDZGj'
+export const discordLink = 'https://discord.gg/qVT8zrUQ'
 
 /** 无协议前缀域名 */
 export const plainTargetDomain = 'nolebase.ayaka.io'
